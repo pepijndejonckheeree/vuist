2b40b070b9d85c5a7d9e5ef2921e025504a1d2f5
pepijndejonckheeree <pepijn.de.jonckheere@student.howest.be>
2020-04-21 21:45:56 +0200

    vuistje function cashe

--- src/functions/show/show.js
+++ src/functions/show/show.js
@@ -12,18 +12,23 @@ const client = contentful.createClient({
 exports.handler = async function(event, context) {
   try {
     const id = event.queryStringParameters.id.replace("/", "")
+    let vuistje;
+    if (storage.has(id)) {
+      console.log("In storage gevonden")
+      vuistje = storage.get(id)
+    }else{
+      const space = await client.getSpace(process.env.CONTENTFUL_SPACE_ID)
+      const environment = await space.getEnvironment("master")
+      const entry = await environment.getEntry(id)
 
-    const space = await client.getSpace(process.env.CONTENTFUL_SPACE_ID)
-    const environment = await space.getEnvironment("master")
-    const entry = await environment.getEntry(id)
-
-    const vuistje = {
-      to: entry.fields.to["en-US"],
-      from: entry.fields.from["en-US"],
-      message: entry.fields.message["en-US"],
+      vuistje = {
+        to: entry.fields.to["en-US"],
+        from: entry.fields.from["en-US"],
+        message: entry.fields.message["en-US"],
+      }
+      console.log("In storage plaatsen")
+      storage.set(id, vuistje)
     }
-
-    
 
     return {
       statusCode: 200,
